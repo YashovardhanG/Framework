152c3b69d40699a85d0579d70bc1182f2719a35e
Yash <yashovardhangoyal@gmail.com>
2019-03-23 17:36:09 +0530

    Remove Spacing

--- JavaExamples/src/arrays/ArrayDemo.java
+++ JavaExamples/src/arrays/ArrayDemo.java
@@ -4,18 +4,15 @@ public class ArrayDemo {
 //comment
 	public static void main(String[] args) {
 		  
-		int a[] = new int[5];
-		
+		int a[] = new int[5];		
 		a[1]= 4;
 		a[2] =5;
 		a[3] = 7 ;
 		a[4] = 4;
 		a[0] = 2;
-		int size = a.length;
-		
+		int size = a.length;		
 		int i=0;
-		while (i< size) {
-			
+		while (i< size) {			
 			
 			System.out.println(a[i]);
 			i++;
